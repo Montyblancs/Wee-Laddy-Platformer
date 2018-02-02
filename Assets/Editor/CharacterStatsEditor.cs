966eb770e0a40a4b6e89341f1fe1b5c436cf08aa
Joe Van Deusen <j.vandeusen@r3education.com>
2018-02-02 16:45:50 -0500

    Saving WIP for Custom inspector
    This will make it vastly easier for testing, so I think its worth while.

--- Assets/Editor/CharacterStatsEditor.cs
+++ Assets/Editor/CharacterStatsEditor.cs
@@ -2,7 +2,8 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections;
  
-[CustomEditor(typeof(CharacterStats))] public class CharacterStatsEditor : Editor
+[CustomEditor(typeof(CharacterStats))]
+public class CharacterStatsEditor : Editor
 {
 	CharacterStats m_Instance;
 	PropertyField[] m_fields;
@@ -19,6 +20,15 @@ using System.Collections;
 			return;
 		this.DrawDefaultInspector();
 		ExposeProperties.Expose(m_fields);
+		// loads the stat set dictionary from the lists to facilitate serialization
 		m_Instance.loadStatSet();
+		// now begin the custom layout for our inspector
+		var emptyOptions = new GUILayoutOption[0];
+		EditorGUILayout.BeginVertical(emptyOptions);
+		EditorGUILayout.BeginHorizontal(emptyOptions);
+		// Going to Test by trying to find a way to get all 3 stat properties into 1 line.
+		// LEFT OFF IN THIS TUTORIAL https://unity3d.com/learn/tutorials/topics/interface-essentials/property-drawers-custom-inspectors
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.EndVertical();
 	}
 }
