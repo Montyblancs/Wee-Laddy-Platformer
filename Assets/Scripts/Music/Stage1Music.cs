457e465813314b11b55657a5f65d4622209640d3
Montyblancs <montyblancs99@gmail.com>
2018-05-08 20:49:59 -0400

    ALMOST perfect looping music with intro

--- Assets/Scripts/Music/Stage1Music.cs
+++ Assets/Scripts/Music/Stage1Music.cs
@@ -7,14 +7,43 @@ public class Stage1Music : MonoBehaviour
 
     public AudioClip StageMusic;
     public AudioClip BossMusic;
+    public int loopDuration;
+    public int loopEnd;
 
     AudioSource thisSource;
 
     // Use this for initialization
     void Start()
     {
-        //This needs two audiosources in order to transition properly. Loading a clip adds overhead, and thus an audable delay in the track.
         thisSource = GetComponent<AudioSource>();
+        //Get length of intro and loop using the debug option below
+        //Debug.Log(thisSource.clip.samples);
+        // Intro duration = 305280
+        // loopDuration - The length of just the looping section of music
+        // loopDuration = 1816704
+        // loopEnd - The length of the entire song + intro. This is where the music should stop and loop.
+        // loopEnd = 2121984
+    }
+
+    //Set to check every 3ms (Edit -> Project Settings -> Time -> Fixed Timestep)
+    //(DO NOT USE THE DEFAULT UNITY PHYSICS ENGINE WITH THIS 3MS SETTING)
+    private void FixedUpdate()
+    {
+        if (loopDuration > 0 && loopEnd > 0)
+        {
+            // * StageMusic.frequency
+            //Debug.Log(thisSource.timeSamples + " | " + loopEnd);
+            if (thisSource.timeSamples > loopEnd)
+            {
+                Debug.Log("looping");
+                thisSource.timeSamples -= loopDuration;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        
     }
 
     public void PlayBossTrack()
@@ -37,6 +66,8 @@ public class Stage1Music : MonoBehaviour
         thisSource.Stop();
         thisSource.volume = startVolume;
         thisSource.clip = nextClip;
+        //thisSource.timeSamples to get current time
+        thisSource.timeSamples = 3;
         thisSource.Play();
     }
 }
